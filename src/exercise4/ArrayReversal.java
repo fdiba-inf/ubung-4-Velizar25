4a8bbef2b377237e9dd297e17c87721e08b1c2e1
Velizar25 <>
2021-11-08 15:26:11 +0000

    Some message

--- src/exercise4/ArrayReversal.java
+++ src/exercise4/ArrayReversal.java
@@ -1,19 +1,24 @@
 package exercise4;
-
 import java.util.Scanner;
 
 public class ArrayReversal {
 
   public static void main(String[] args) {
+
     Scanner input = new Scanner(System.in);
-    char a[] = new char[5];
-    a[0] = 'F';
-    a[1] = 'D';
-    a[2] = 'I';
-    a[3] = 'B';
-    a[4] = 'A';
-    for (int i = 0; i < a.length; i++) {
-        System.out.println(a[i]);
-      }   
+    int size = input.nextInt();
+    char ch[] = new char[size];
+    for (int i = 0; i < size; i++) {
+      ch[i] = input.next().charAt(0);
+    }
+    System.out.print("Reversed symbols: [");
+    for (int i = size - 1; i >= 0; i--) {
+      if (i > 0) {
+        System.out.print(ch[i] + ", ");
+      } else {
+        System.out.print(ch[i]);
+      }
+    }
+    System.out.print("]");
   }
 }
